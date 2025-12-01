7e09963e6280a1b25670d2984e841d6558a280ad
Lev Koliadich <lkolyadich@gmail.com>
2025-12-01 17:29:34 +0100

    day 1 part 2

--- src/adevent_of_code_2025/day_01.clj
+++ src/adevent_of_code_2025/day_01.clj
@@ -8,27 +8,35 @@
 (defrecord RotationResult [value wraps])
 
 (defn parse-rotation [line]
-  (let [direction (subs line 0 1)
+  (let [direction (case (get line 0) \R :right \L :left)
         distance (Integer/parseInt (subs line 1))]
     (->Rotation direction distance)))
 
+(defn rotation-to-angle [rotation]
+  ((case (:direction rotation) :right + :left -) (:distance rotation)))
+
+(defn on [f g] #(apply f (map g %&)))
+
+(defn of [f g] #(g (apply f %&)))
+
 (defn rotate [current rotation]
-  (let [func (case (:direction rotation) "R" + "L" -)
-        after (func (:value current) (:distance rotation))
-        cropped (mod after 100)
-        wraps ((if (neg? after) inc identity) (abs (quot after 100)))]
-    (->RotationResult cropped wraps)))
+  (let [before (:value current)
+        after (+ before (rotation-to-angle rotation))
+        modif (case (:direction rotation) :right identity :left dec)
+        wrapped-times #(-> % modif (floor-div 100))
+        wraps ((on (of - abs) wrapped-times) before after)]
+    (->RotationResult after wraps)))
 
 (defn count-zeros [coll]
-  (reduce #(if (== (:value %2) 0) (inc %1) %1) 0 coll))
+  (count (filter #(-> % (mod 100) zero?) (map :value coll))))
 
-(defn count-wraps [coll] (reduce + 0 (map :wraps coll)))
+(defn count-wraps [coll] (apply + (map :wraps coll)))
 
 (defn solution [file]
-  (println
-   ; (count-zeros
-   (count-wraps
-    (reductions rotate (->RotationResult 50 0)
-                (map parse-rotation
-                     (split-lines
-                      (slurp file)))))))
+  (let [scan (reductions rotate (->RotationResult 50 0)
+                         (map parse-rotation
+                              (split-lines
+                               (slurp file))))]
+    (println
+     (count-zeros scan)
+     (count-wraps scan))))
