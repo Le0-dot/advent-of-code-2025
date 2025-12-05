29d5e7cd49b9d528b33f9ca5580603d785a2f716
Lev Koliadich <lkolyadich@gmail.com>
2025-12-05 12:47:33 +0100

    day 5 part 2

--- src/advent_of_code_2025/day_05.clj
+++ src/advent_of_code_2025/day_05.clj
@@ -2,13 +2,32 @@
     (:require
      [clojure.string :refer [split split-lines]]))
 
-(defrecord InclusiveRange [start end])
+(defrecord InclusiveRange [start end]
+  java.lang.Comparable
+  (compareTo [self other] (compare start (:start other))))
 
 (defn in-range? [^InclusiveRange {:keys [start end]} n]
   (<= start n end))
 
 (defn in-any-range? [coll n]
   (some #(in-range? % n) coll))
+
+(defn overlap? [^InclusiveRange left ^InclusiveRange right]
+  (or (in-range? left (:start right))
+      (in-range? left (:end right))
+      (in-range? right (:start left))
+      (in-range? right (:end left))))
+
+(defn join-ranges [^InclusiveRange left ^InclusiveRange right]
+  (->InclusiveRange
+   (min (:start left) (:start right))
+   (max (:end left) (:end right))))
+
+(defn ranges-cons [coll new-range]
+  (let [{overlapping true other false} (group-by #(overlap? % new-range) coll)]
+    (cons
+     (reduce join-ranges new-range overlapping)
+     other)))
 
 (defn parse-range-line [line]
   (apply ->InclusiveRange (map #(Long/parseLong %) (split line #"-"))))
@@ -25,7 +44,13 @@
                              slurp
                              split-lines
                              parse-lines)]
-    (->> numbers
-         (filter #(in-any-range? fresh %))
-         count
+    ; (->> numbers
+    ;      (filter #(in-any-range? fresh %))
+    ;      count
+    ;      println)))
+    (->> fresh
+         sort
+         (reduce ranges-cons '())
+         (map #(- (:end %) (:start %) -1))
+         (apply +)
          println)))
