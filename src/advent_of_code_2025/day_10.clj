dc39f8799adba43a917f972268f760c7f48d96b5
Lev Koliadich <lkolyadich@gmail.com>
2025-12-10 13:21:39 +0100

    day 10 part 2 (ran out of memory)

--- src/advent_of_code_2025/day_10.clj
+++ src/advent_of_code_2025/day_10.clj
@@ -16,30 +16,64 @@
    #(Integer/parseInt %)
    (split (subs s 1 (dec (count s))) #",")))
 
+(defn parse-jolatge [s]
+  (map
+   #(Integer/parseInt %)
+   (split (subs s 1 (dec (count s))) #",")))
+
 (defn parse-line [line]
   (let [elements (split line #" ")]
     {:indicator (parse-indicator (first elements))
-     :buttons   (map parse-button (butlast (rest elements)))}))
+     :buttons   (map parse-button (butlast (rest elements)))
+     :jolatge   (parse-jolatge (last elements))}))
 
-(defn toggle [state n]
+; (defn toggle [state n]
+;   (concat
+;    (take n state)
+;    (list (if (= :on (nth state n)) :off :on))
+;    (drop (inc n) state)))
+;
+; (defn press [state button]
+;   (reduce toggle state button))
+;
+; (defn act
+;   ([machine] (act machine
+;                   (conj PersistentQueue/EMPTY (vector (repeat (count (:indicator machine)) :off) 0))
+;                   #{}))
+;   ([machine queue visited]
+;    (let [[current n] (peek queue)
+;          new-states (remove visited (map #(press current %) (:buttons machine)))]
+;      (cond
+;        (visited current) (recur machine (pop queue) visited)
+;        (some #{(:indicator machine)} new-states) (inc n)
+;        :else (recur
+;               machine
+;               (apply conj (pop queue) (map #(vector % (inc n)) new-states))
+;               (conj visited current))))))
+
+(defn increase [state n]
   (concat
    (take n state)
-   (list (if (= :on (nth state n)) :off :on))
+   (list (inc (nth state n)))
    (drop (inc n) state)))
 
 (defn press [state button]
-  (reduce toggle state button))
+  (reduce increase state button))
+
+(defn invalid? [state jolatge]
+  (some identity (map > state jolatge)))
 
 (defn act
-  ([machine] (act machine
-                  (conj PersistentQueue/EMPTY (vector (repeat (count (:indicator machine)) :off) 0))
-                  #{}))
+  ([machine] (act
+              machine
+              (conj PersistentQueue/EMPTY (vector (repeat (count (:indicator machine)) 0) 0))
+              #{}))
   ([machine queue visited]
    (let [[current n] (peek queue)
-         new-states (remove visited (map #(press current %) (:buttons machine)))]
+         new-states (remove #(invalid? % (:jolatge machine)) (map #(press current %) (:buttons machine)))]
      (cond
        (visited current) (recur machine (pop queue) visited)
-       (some #{(:indicator machine)} new-states) (inc n)
+       (some #{(:jolatge machine)} new-states) (inc n)
        :else (recur
               machine
               (apply conj (pop queue) (map #(vector % (inc n)) new-states))
